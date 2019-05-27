2312b2e8d595fbd35699bd5746ce07af42d5aeeb
UtopiaBeam <utopiabeam@gmail.com>
2019-05-27 20:19:57 +0700

    :lipstick: Attach navbar to bottom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Tagline from '../components/Tagline';
+import Navbar from '../components/Navbar';
+import Content from '../components/Content';
 import './Home.module.scss';
 
 export default () => {
   return (
     <div>
       <Tagline />
+      <Navbar />
+      <Content />
     </div>
   );
 };
