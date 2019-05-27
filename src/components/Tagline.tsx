17b055251f76b8199b2e0a70f0b195810bfa1ce6
UtopiaBeam <utopiabeam@gmail.com>
2019-05-27 20:07:46 +0700

    :lipstick: Expand tagline width to 100%

--- src/components/Tagline.tsx
+++ src/components/Tagline.tsx
@@ -4,11 +4,14 @@ import styles from './Tagline.module.scss';
 export default () => {
   return (
     <div className={styles.wrapper}>
-      <h1>Welcome to my blog!</h1>
-      <p>
-      MWIT 24/4 | CU 101 | CPCU 44 | President of Thinc. | Back-end developer |
-      Interesting in DevOps & front-end development | Love to learn new things
-      </p>
+      <div className={styles.text}>
+        <h1>Welcome to my blog!</h1>
+        <p>
+          MWIT 24/4 | CU 101 | CPCU 44 | President of Thinc. | Back-end
+          developer | Interesting in DevOps & front-end development | Love to
+          learn new things
+        </p>
+      </div>
     </div>
   );
 };
