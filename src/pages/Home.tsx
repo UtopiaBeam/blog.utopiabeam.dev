e1ef4a11728141aff719ec03fc10d9e23e5ba0e4
UtopiaBeam <utopiabeam@gmail.com>
2019-05-27 16:20:00 +0700

    :sparkles: Add tagline

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import Content from '../components/Content';
+import styles from './Home.module.scss';
 
 export default () => {
   return (
     <div>
-      <Navbar />
+      <div className={styles.text}>
+        <h1>Welcome to my blog!</h1>
+        <p>
+          Hello! My name is UtopiaBeam. I'm back-end developer who is trying
+          to be full-stack developer. I'm also interested in math and algorithms.
+        </p>
+      </div>
     </div>
   );
 };
