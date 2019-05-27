5d5e192df2a8e1aa6d634c7fe45d4d55c02f337e
UtopiaBeam <utopiabeam@gmail.com>
2019-05-27 20:20:07 +0700

    :sparkles: Add Content

---
+++ src/components/Content.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Content.module.scss';
+
+export default () => {
+    return (
+        <div className={styles.content}>
+            <h1>Newest Blog</h1>
+        </div>
+    )
+}
