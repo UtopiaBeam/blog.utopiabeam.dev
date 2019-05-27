a0e2aed3b72bc9b4fff0ea18fed8b57e500cf235
UtopiaBeam <utopiabeam@gmail.com>
2019-05-27 19:25:19 +0700

    :lipstick: Fix tagline

---
+++ src/components/Tagline.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Tagline.module.scss';
+
+export default () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Welcome to my blog!</h1>
+      <p>
+      MWIT 24/4 | CU 101 | CPCU 44 | President of Thinc. | Back-end developer |
+      Interesting in DevOps & front-end development | Love to learn new things
+      </p>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import Content from '../components/Content';
-import styles from './Home.module.scss';
+import Tagline from '../components/Tagline';
+import './Home.module.scss';
 
 export default () => {
   return (
     <div>
-      <div className={styles.text}>
-        <h1>Welcome to my blog!</h1>
-        <p>
-          Hello! My name is UtopiaBeam. I'm back-end developer who is trying
-          to be full-stack developer. I'm also interested in math and algorithms.
-        </p>
-      </div>
+      <Tagline />
     </div>
   );
 };
