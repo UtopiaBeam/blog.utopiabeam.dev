ee0ed91006437b361b3b7929f09fe3f214ee1898
UtopiaBeam <utopiabeam@gmail.com>
2019-05-31 20:48:49 +0700

    :construction: Working on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Navbar.module.scss';
+import { Link } from 'react-router-dom';
 
 export default () => {
+  const [initialized, setInitialized] = useState(false);
+  const [fixed, setFixed] = useState(false);
+
+  function handleScroll(): void {
+    setFixed(window.pageYOffset > window.innerHeight);
+  }
+
+  useEffect((): void => {
+    if (!initialized) {
+      window.addEventListener('scroll', handleScroll);
+      setInitialized(true);
+    }
+  }, [initialized]);
+
   return (
-    <ul className={styles.ul}>
+    <ul className={fixed ? styles.fixedNavbar : styles.navbar}>
+      <li className={styles.logo}>
+        <a href="https://utopiabeam.dev">{'{UtopiaBeam}'}</a>
+      </li>
       <li className={styles.li}>
-        <a href="https://utopiabeam.dev">home</a>
+        <Link to="/">Home</Link>
       </li>
       <li className={styles.li}>
         <a href="https://osu.ppy.sh/u/UtopiaBeam">osu!</a>
