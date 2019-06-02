ee239cf1be73837e11a0a87ae835dc9c961ecd22
UtopiaBeam <utopiabeam@gmail.com>
2019-06-02 16:49:48 +0700

    :art: Run prettier

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './Content.module.scss';
 
 export default () => {
-    return (
-        <div className={styles.content}>
-            <h1>Newest Blogs</h1>
-        </div>
-    )
-}
+  return (
+    <div className={styles.content}>
+      <h1>Newest Blogs</h1>
+    </div>
+  );
+};
