336dfb3a2a3d3d45ac855e6779f6d80ad5ce05fb
UtopiaBeam <utopiabeam@gmail.com>
2019-06-02 17:30:42 +0700

    :sparkles: Add 404 page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, RouteProps } from 'react-router-dom';
 import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 
 const routes: RouteProps[] = [
   {
     path: '/',
     component: Home,
     exact: true,
-  }
+  },
+  {
+    path: '',
+    component: NotFound,
+  },
 ];
 
 export default routes.map((prop, index) => <Route key={index} {...prop} />);
