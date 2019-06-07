bde3cb10cb8898b00556e85bf87c403513949b42
UtopiaBeam <utopiabeam@gmail.com>
2019-06-07 11:02:49 +0700

    :lipstick: Update navbar style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, FormEvent } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Navbar.module.scss';
 import { Link } from 'react-router-dom';
 
