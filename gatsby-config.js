fa6ca9d6131f9de6e816b8de8d3675810d148550
UtopiaBeam <utopiabeam@gmail.com>
2019-07-28 17:47:54 +0700

    :wrench: Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,28 +8,28 @@ module.exports = {
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `blogs`,
+        path: `${__dirname}/data/blog`,
       },
     },
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,51 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+const POST_PER_PAGE = 10;
+
+// exports.onCreateNode = ({ node, getNode }) => {
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const fileNode = getNode(node.parent);
+//     console.log(`\n`, fileNode.relativePath);
+//   }
+// };
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+        edges {
+          node {
+            id
+            html
+            frontmatter {
+              title
+              slug
+              date(formatString: "MMM DD, YYYY")
+              description
+              banner
+              featured
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const { id, frontmatter, html } = node;
+    const { title, slug, description, date, banner } = frontmatter;
+    createPage({
+      path: slug,
+      component: path.resolve('src/templates/BlogPost.tsx'),
+      context: {
+        title,
+        slug,
+        description,
+        date,
+        banner,
+        html,
+      },
+    });
+  });
+};
