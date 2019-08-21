bf2fe0bdcd9289416c03eb9bc1dc5f8ad2aa27b7
UtopiaBeam <utopiabeam@gmail.com>
2019-08-21 23:26:18 +0700

    :poop: :lipstick: Update Card props & components

--- src/templates/TagList.tsx
+++ src/templates/TagList.tsx
@@ -4,6 +4,7 @@ import { createGlobalStyle } from 'styled-components';
 import { Flex, Box } from 'rebass';
 import Pagination from '../components/Pagination';
 import Card from '../components/Card';
+import Header from '../components/Header';
 
 interface Tag {
   title: string;
@@ -34,12 +35,13 @@ export default (props: Props) => {
   const { numPage, currentPage, tags } = props.pageContext;
   const tagCards = tags.map((tag: Tag) => (
     <Box width={[1, 1, 1 / 2]}>
-      <Card {...tag} />
+      <Card {...tag} pathPrefix="tag" />
     </Box>
   ));
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Flex justifyContent="center">
         <Box width={[1, 2 / 3]}>
           <Flex flexWrap="wrap">{tagCards}</Flex>
