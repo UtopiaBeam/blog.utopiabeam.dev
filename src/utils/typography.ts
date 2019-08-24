f8b529d6fc6e9045ff6a1d9c65566c77fffca941
UtopiaBeam <utopiabeam@gmail.com>
2019-08-24 14:46:53 +0700

    :wrench: Change typography config

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -2,17 +2,12 @@ import Typography from 'typography';
 
 const typography = new Typography({
   baseFontSize: '18px',
-  baseLineHeight: 1.5,
+  baseLineHeight: 1.7,
   headerFontFamily: ['Athiti', 'sans-serif'],
-  bodyFontFamily: ['Pridi', 'sans-serif'],
   googleFonts: [
     {
       name: 'Athiti',
       styles: ['600', ' 700'],
-    },
-    {
-      name: 'Pridi',
-      styles: ['400'],
     },
     {
       name: 'Catamaran',
