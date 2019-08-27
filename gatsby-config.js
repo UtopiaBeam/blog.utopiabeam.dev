ba8e1d7d825aa88e78b9ea4bed2289a2d3de5e46
UtopiaBeam <utopiabeam@gmail.com>
2019-08-27 23:42:48 +0700

    :card_file_box: Add stupid-hackathon-th3 blog
    - add `gatsby-remark-embed-video` and `gatsby-remark-responsive-iframe`

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,8 @@ module.exports = {
               strict: `ignore`,
             },
           },
+          `gatsby-remark-embed-video`,
+          `gatsby-remark-responsive-iframe`,
         ],
       },
     },
