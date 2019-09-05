0f707a35902a5f080a8ef99622f2544a049abfde
UtopiaBeam <utopiabeam@gmail.com>
2019-09-05 13:26:16 +0700

    :sparkles: Add share button

--- src/html.tsx
+++ src/html.tsx
@@ -37,6 +37,7 @@ export default (props: Props) => {
         />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon-precomposed" href="/favicon.ico" />
+        <script src="https://kit.fontawesome.com/2c38a4903c.js" />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
